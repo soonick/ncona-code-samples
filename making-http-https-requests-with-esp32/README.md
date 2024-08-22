094ca276689e8fd239e4f5da9725ab8e2508ce94
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2024-08-22 22:52:41 +0200

    Production example for making https reqeusts with esp32

--- making-http-https-requests-with-esp32/README.md
+++ making-http-https-requests-with-esp32/README.md
@@ -1,15 +1,15 @@
 # Introduction to ESP32 WiFi Manager
 
-Before you running the examples, replace `<YOUR_SSID>` and `<YOUR_PASSWORD>` with your SSID and password in `minimal-https/main/main.cpp`.
+Before you running the examples, replace `<YOUR_SSID>` and `<YOUR_PASSWORD>` with your SSID and password in `minimal-https/main/main.cpp` and `production-https/main/main.cpp`.
 
-To build the example:
-
-```
-make build
-```
-
-To upload it to ESP32 and start serial monitor:
+To build and upload the minimal example to ESP32 and start serial monitor:
 
 ```
 make upload serial
+```
+
+To build and upload the production example to ESP32 and start serial monitor:
+
+```
+make upload-prod serial-prod
 ```
--- making-http-https-requests-with-esp32/production-https/main/main.cpp
+++ making-http-https-requests-with-esp32/production-https/main/main.cpp
@@ -1,10 +1,14 @@
 #include "esp_crt_bundle.h"
+#include "esp_err.h"
 #include "esp_http_client.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
+#include <fcntl.h>
 #include <sys/param.h>
 
-static const char *TAG = "minimal-https";
+static esp_netif_t *wifi_if = NULL;
+static const char *TAG = "production-https";
+static SemaphoreHandle_t IP_SEMPH = NULL;
 #define SSID "<YOUR_SSID>"
 #define PASSWORD "<YOUR_PASSWORD>"
 
@@ -70,29 +74,30 @@ esp_err_t http_event_handler(esp_http_client_event_t *evt) {
   return ESP_OK;
 }
 
-static void https_with_url() {
-  esp_http_client_config_t config = {
-      .url = "https://ncona.com/about-me/",
-      .event_handler = http_event_handler,
-      .crt_bundle_attach = esp_crt_bundle_attach,
-  };
-  esp_http_client_handle_t client = esp_http_client_init(&config);
-  esp_http_client_perform(client);
+static void got_ip_handler(void *arg, esp_event_base_t event_base,
+                           int32_t event_id, void *event_data) {
+  ESP_LOGI(TAG, "Got IP address");
+  xSemaphoreGive(IP_SEMPH);
 }
 
 esp_err_t connect_to_ap() {
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   cfg.nvs_enable = 0;
-  esp_wifi_init(&cfg);
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
   esp_netif_inherent_config_t esp_netif_config =
       ESP_NETIF_INHERENT_DEFAULT_WIFI_STA();
-  esp_netif_create_wifi(WIFI_IF_STA, &esp_netif_config);
+  wifi_if = esp_netif_create_wifi(WIFI_IF_STA, &esp_netif_config);
   esp_wifi_set_default_wifi_sta_handlers();
 
-  esp_wifi_set_storage(WIFI_STORAGE_RAM);
-  esp_wifi_set_mode(WIFI_MODE_STA);
-  esp_wifi_start();
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  IP_SEMPH = xSemaphoreCreateBinary();
+  if (IP_SEMPH == NULL) {
+    return ESP_ERR_NO_MEM;
+  }
 
   wifi_config_t wifi_config = {
       .sta =
@@ -103,16 +108,55 @@ esp_err_t connect_to_ap() {
   };
   ESP_LOGI(TAG, "Connecting to %s...", wifi_config.sta.ssid);
   esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+
+  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
+                                             &got_ip_handler, NULL));
+
   esp_wifi_connect();
+
+  xSemaphoreTake(IP_SEMPH, portMAX_DELAY);
 
   return ESP_OK;
 }
 
+static void stop_wifi() {
+  ESP_LOGI(TAG, "Turning off wifi");
+  ESP_ERROR_CHECK(esp_wifi_stop());
+  ESP_ERROR_CHECK(esp_wifi_deinit());
+  ESP_ERROR_CHECK(esp_wifi_clear_default_wifi_driver_and_handlers(wifi_if));
+  esp_netif_destroy(wifi_if);
+  wifi_if = NULL;
+}
+
+static void https_with_url(void *pvParameters) {
+  while (true) {
+    ESP_ERROR_CHECK(connect_to_ap());
+
+    esp_http_client_config_t config = {
+        .url = "https://ncona.com/about-me/",
+        .event_handler = http_event_handler,
+        .crt_bundle_attach = esp_crt_bundle_attach,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK) {
+      int status_code = esp_http_client_get_status_code(client);
+      if (status_code != 200) {
+        ESP_LOGI(TAG, "Got %d code", status_code);
+      }
+    } else {
+        ESP_LOGE(TAG, "Error with https request: %s", esp_err_to_name(err));
+    }
+
+    ESP_ERROR_CHECK(esp_http_client_cleanup(client));
+    stop_wifi();
+    vTaskDelay(60000 / portTICK_PERIOD_MS);
+  }
+}
+
 extern "C" void app_main() {
-  esp_netif_init();
-  esp_event_loop_create_default();
-  connect_to_ap();
-  vTaskDelay(10000 / portTICK_PERIOD_MS);
-  https_with_url();
-  vTaskDelay(10000 / portTICK_PERIOD_MS);
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  xTaskCreate(&https_with_url, "https_with_url", 8192, NULL, 5, NULL);
 }
